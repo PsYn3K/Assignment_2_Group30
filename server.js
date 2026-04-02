36e3af52979c2b04e95d0e2a6af8db5474dd5a9f
Karl Lansang <lans0013@algonquinlive.com>
2026-04-02 16:02:10 -0400

    added server and auth setup

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const path = require("path");
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/taskmanager")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Middleware
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+  secret: "secret123",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Routes
+app.use("/", authRoutes);
+
+// Home
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
